011f40b8c0c01fd71177006c0925bbd46efdb042
Daniel Evans <daniel.evans2@cogniant.com>
2019-07-26 11:16:19 -0400

    intellij sux

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 import java.util.TimeZone;
 
